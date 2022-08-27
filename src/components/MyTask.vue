560a3697c151c1284bf683fc8ced8ae0d8246f66
mirekk42 <112110626+mirekk42@users.noreply.github.com>
2022-08-27 17:30:06 +0200

    Develop (#1)
    * commit 1 dev
    
    * page 1 timer i task
    
    * Task component

---
+++ src/components/MyTask.vue
@@ -0,0 +1,51 @@
+<template>
+   <div class='cardBox'>
+    <div class='container tasker'>
+      <strong>My task is: {{ task }}</strong><br>
+      <input 
+        type='text'
+        v-model='task'
+        class='taskInput' />
+        <button v-on:click='addTask'> Add Task </button>
+    </div>
+   </div>
+</template>
+
+<script>
+export default{
+name : 'TaskInput',
+data: () => ({
+    task: 'wpisz...',
+  }),
+methods: {
+    addTask(){
+      this.$emit('add-task', this.task);
+      this.task = '';
+    },
+  },
+}
+</script>
+
+<style>
+.cardBox{
+max-width: 100%;
+max-height: 25%;
+text-align: center;
+}
+.tasker{
+    margin: 20px;
+    font-size: xx-large;
+  }
+  .tasker .taskInput {
+    font-size: 14px;
+    margin: 0 10px;
+    border: 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.75);
+  }
+  .tasker button {
+    border: 1px solid rgba(0, 0, 0, 0.75);
+    border-radius: 3px;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2);
+  }
+
+</style>
---
+++ src/views/Page1.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+<div>
+  <div class='cardBox'>
+    <div class='container'>
+      <h2>Today is:</h2>
+      <h3>{{ getCurrentDate }}</h3>
+    </div>
+  </div>
+  </div>
+  <task-input @add-task="addNewTask"></task-input>
+</div>
+</template>
+
+<script>
+import TaskInput from '../components/MyTask.vue'
+
+
+export default {
+  name: 'CurrentTime',
+  components: {TaskInput},
+   methods:{
+    addNewTask(task){
+      alert(`New task added: ${task}`);
+    },
+  },
+  computed: {
+    getCurrentDate() {
+      const browserLocale =
+        navigator.languages && navigator.languages.length
+          ? navigator.languages[0]
+          : navigator.language;
+      const intlDateTime = new Intl.DateTimeFormat(
+        browserLocale, 
+        {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric'
+        });
+
+      return intlDateTime.format(new Date());
+    }
+  },
+};
+</script>
+
+<style scoped>
+.cardBox{
+max-width: 100%;
+max-height: 25%;
+text-align: center;
+}
+
+</style>
