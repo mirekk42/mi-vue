1a47e77ce6b49663f463307252c206f693fbad79
mirekk42 <mi2@onet.eu>
2022-08-27 20:35:35 +0200

    current-time component

--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -1,56 +1,27 @@
 <template>
 <div>
-<div>
-  <div class='cardBox'>
-    <div class='container'>
-      <h2>Today is:</h2>
-      <h3>{{ getCurrentDate }}</h3>
-    </div>
-  </div>
-  </div>
+  <current-time></current-time>
   <task-input @add-task="addNewTask"></task-input>
 </div>
 </template>
 
 <script>
 import TaskInput from '../components/MyTask.vue'
+import CurrentTime from '../components/CurrentTime.vue';
 
 
 export default {
-  name: 'CurrentTime',
-  components: {TaskInput},
+  name: 'Main',
+  components: {TaskInput,CurrentTime},
    methods:{
     addNewTask(task){
       alert(`New task added: ${task}`);
     },
   },
-  computed: {
-    getCurrentDate() {
-      const browserLocale =
-        navigator.languages && navigator.languages.length
-          ? navigator.languages[0]
-          : navigator.language;
-      const intlDateTime = new Intl.DateTimeFormat(
-        browserLocale, 
-        {
-          year: 'numeric',
-          month: 'numeric',
-          day: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric'
-        });
-
-      return intlDateTime.format(new Date());
-    }
-  },
+ 
 };
 </script>
 
 <style scoped>
-.cardBox{
-max-width: 100%;
-max-height: 25%;
-text-align: center;
-}
 
 </style>
