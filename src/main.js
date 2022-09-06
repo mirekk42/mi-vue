42c6e6b5ec90560171fed4082c2acc1260097845
mirekk42 <mi2@onet.eu>
2022-09-06 16:52:17 +0200

    server mirage

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './server/server'
+
 
 import './assets/main.css'
 import './assets/style.css'
